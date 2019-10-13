371b9add6eaf73f05c3cc8345e969d1578050909
Wong Hoi Sing Edison <hswong3i@gmail.com>
2019-10-13 17:32:43 +0800

    chown $BITBUCKET_OWNER:$BITBUCKET_GROUP $BITBUCKET_HOME

--- files/usr/local/bin/docker-entrypoint.sh
+++ files/usr/local/bin/docker-entrypoint.sh
@@ -10,7 +10,7 @@ fi
 # Allow the container to be stated with `--user`
 if [ "$1" = '/opt/atlassian/bitbucket/bin/start-bitbucket.sh' ] && [ "$(id -u)" = '0' ]; then
     mkdir -p $BITBUCKET_HOME
-    chown -Rf $BITBUCKET_OWNER:$BITBUCKET_GROUP $BITBUCKET_HOME
+    chown $BITBUCKET_OWNER:$BITBUCKET_GROUP $BITBUCKET_HOME
     chmod 0755 $BITBUCKET_HOME
     exec gosu $BITBUCKET_OWNER "$BASH_SOURCE" "$@"
 fi
